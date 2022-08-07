282116fd344c64b7c986d69adf8c19dd102d8c0a
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-08 02:48:36 +0530

    adding all unversioned files

---
+++ src/main/java/com/springpractice/recipedemo/RecipedemoApplication.java
@@ -0,0 +1,13 @@
+package com.springpractice.recipedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipedemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/springpractice/recipedemo/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package com.springpractice.recipedemo.domain;
+
+public enum Difficulty {
+    EASY, MODERATE, HARD
+}
