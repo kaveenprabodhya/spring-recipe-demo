24fd72b8c34ec0a0c22fcf26a6eaba978f4c306d
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-08 20:44:14 +0530

    lombok impl

--- src/main/java/com/springpractice/recipedemo/controllers/IndexController.java
+++ src/main/java/com/springpractice/recipedemo/controllers/IndexController.java
@@ -5,12 +5,14 @@ import com.springpractice.recipedemo.domain.UnitOfMeasure;
 import com.springpractice.recipedemo.repositories.CategoryRepository;
 import com.springpractice.recipedemo.repositories.UnitOfMeasureRepository;
 import com.springpractice.recipedemo.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
 
+@Slf4j
 @Controller
 public class IndexController {
     private RecipeService recipeService;
@@ -21,6 +23,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index.html", "/index"})
     public String getIndexPage(Model model){
+        log.debug("Getting index page.");
         model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
