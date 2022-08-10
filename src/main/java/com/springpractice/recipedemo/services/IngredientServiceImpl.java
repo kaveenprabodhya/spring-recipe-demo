57d766eb661e5cbc96431f3884c9bb38512b50f1
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-11 00:58:48 +0530

    impl saveIngredientCommand in IngredientServiceImpl

--- src/main/java/com/springpractice/recipedemo/services/IngredientServiceImpl.java
+++ src/main/java/com/springpractice/recipedemo/services/IngredientServiceImpl.java
@@ -1,11 +1,15 @@
 package com.springpractice.recipedemo.services;
 
 import com.springpractice.recipedemo.commands.IngredientCommand;
+import com.springpractice.recipedemo.converters.IngredientCommandToIngredient;
 import com.springpractice.recipedemo.converters.IngredientToIngredientCommand;
+import com.springpractice.recipedemo.domain.Ingredient;
 import com.springpractice.recipedemo.domain.Recipe;
 import com.springpractice.recipedemo.repositories.RecipeRepository;
+import com.springpractice.recipedemo.repositories.UnitOfMeasureRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -13,11 +17,18 @@ import java.util.Optional;
 @Service
 public class IngredientServiceImpl implements IngredientService{
     private final IngredientToIngredientCommand ingredientToIngredientCommand;
+    private final IngredientCommandToIngredient ingredientCommandToIngredient;
     private final RecipeRepository recipeRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
 
-    public IngredientServiceImpl(IngredientToIngredientCommand ingredientToIngredientCommand, RecipeRepository recipeRepository) {
+    public IngredientServiceImpl(IngredientToIngredientCommand ingredientToIngredientCommand,
+                                 IngredientCommandToIngredient ingredientCommandToIngredient,
+                                 RecipeRepository recipeRepository,
+                                 UnitOfMeasureRepository unitOfMeasureRepository) {
         this.ingredientToIngredientCommand = ingredientToIngredientCommand;
+        this.ingredientCommandToIngredient = ingredientCommandToIngredient;
         this.recipeRepository = recipeRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
     }
 
     @Override
@@ -42,4 +53,44 @@ public class IngredientServiceImpl implements IngredientService{
 
         return ingredientCommandOptional.get();
     }
+
+    @Override
+    @Transactional
+    public IngredientCommand saveIngredientCommand(IngredientCommand command) {
+        Optional<Recipe> recipeOptional = recipeRepository.findById(command.getRecipeId());
+
+        if(!recipeOptional.isPresent()){
+            log.error("Recipe not found for id: " + command.getRecipeId());
+            return new IngredientCommand();
+        } else {
+            Recipe recipe = recipeOptional.get();
+
+            Optional<Ingredient> ingredientOptional = recipe
+                    .getIngredients()
+                    .stream()
+                    .filter(ingredient -> ingredient.getId().equals(command.getId()))
+                    .findFirst();
+
+            if(ingredientOptional.isPresent()){
+                Ingredient ingredientFound = ingredientOptional.get();
+                ingredientFound.setDescription(command.getDescription());
+                ingredientFound.setAmount(command.getAmount());
+                ingredientFound.setUom(unitOfMeasureRepository
+                        .findById(command.getUom().getId())
+                        .orElseThrow(() -> new RuntimeException("UOM NOT FOUND")));
+            } else {
+                //add new Ingredient
+                recipe.addIngredient(ingredientCommandToIngredient.convert(command));
+            }
+
+            Recipe savedRecipe = recipeRepository.save(recipe);
+
+            //to do check for fail
+            return ingredientToIngredientCommand.convert(savedRecipe.getIngredients().stream()
+                    .filter(recipeIngredients -> recipeIngredients.getId().equals(command.getId()))
+                    .findFirst()
+                    .get());
+        }
+    }
+
 }
