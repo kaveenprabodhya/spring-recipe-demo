199e385b19989718621ae58627e172ffa2f691ac
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-11 02:37:26 +0530

    add deleteById to IngredientService interface

--- src/main/java/com/springpractice/recipedemo/services/IngredientService.java
+++ src/main/java/com/springpractice/recipedemo/services/IngredientService.java
@@ -4,6 +4,6 @@ import com.springpractice.recipedemo.commands.IngredientCommand;
 
 public interface IngredientService {
     IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
-
     IngredientCommand saveIngredientCommand(IngredientCommand command);
+    void deleteById(Long recipeId, Long idToDelete);
 }
