8647f2e273a6dcac82108ee47e6eba0ad838bf6c
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-11 00:50:39 +0530

    impl unit of measure service interface

---
+++ src/main/java/com/springpractice/recipedemo/services/UnitOfMeasureServiceImpl.java
@@ -0,0 +1,29 @@
+package com.springpractice.recipedemo.services;
+
+import com.springpractice.recipedemo.commands.UnitOfMeasureCommand;
+import com.springpractice.recipedemo.converters.UnitOfMeasureToUnitOfMeasureCommand;
+import com.springpractice.recipedemo.repositories.UnitOfMeasureRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
+public class UnitOfMeasureServiceImpl implements UnitOfMeasureService{
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+    private final UnitOfMeasureToUnitOfMeasureCommand unitOfMeasureToUnitOfMeasureCommand;
+
+    public UnitOfMeasureServiceImpl(UnitOfMeasureRepository unitOfMeasureRepository, UnitOfMeasureToUnitOfMeasureCommand unitOfMeasureToUnitOfMeasureCommand) {
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+        this.unitOfMeasureToUnitOfMeasureCommand = unitOfMeasureToUnitOfMeasureCommand;
+    }
+
+    @Override
+    public Set<UnitOfMeasureCommand> listAllUoms() {
+        return StreamSupport.stream(unitOfMeasureRepository.findAll()
+                        .spliterator(), false)
+                .map(unitOfMeasureToUnitOfMeasureCommand::convert)
+                .collect(Collectors.toSet());
+    }
+}
