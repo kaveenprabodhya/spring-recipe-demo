9d9be47e3248192eb2430a8fdd5bb1bd660a1e02
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-11 02:16:51 +0530

    add newIngredient method in IngredientController

--- src/main/java/com/springpractice/recipedemo/controllers/IngredientController.java
+++ src/main/java/com/springpractice/recipedemo/controllers/IngredientController.java
@@ -1,6 +1,8 @@
 package com.springpractice.recipedemo.controllers;
 
 import com.springpractice.recipedemo.commands.IngredientCommand;
+import com.springpractice.recipedemo.commands.RecipeCommand;
+import com.springpractice.recipedemo.commands.UnitOfMeasureCommand;
 import com.springpractice.recipedemo.services.IngredientService;
 import com.springpractice.recipedemo.services.RecipeService;
 import com.springpractice.recipedemo.services.UnitOfMeasureService;
@@ -39,6 +41,27 @@ public class IngredientController {
                                        @PathVariable String id, Model model){
         model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(Long.valueOf(recipeId), Long.valueOf(id)));
         return "recipe/ingredient/show";
+    }
+
+    @GetMapping
+    @RequestMapping("recipe/{recipeId}/ingredient/new")
+    public String newIngredient(@PathVariable String recipeId, Model model){
+
+        //make sure we have a good id value
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(recipeId));
+        //todo raise exception if null
+
+        //need to return back parent id for hidden form property
+        IngredientCommand ingredientCommand = new IngredientCommand();
+        ingredientCommand.setRecipeId(Long.valueOf(recipeId));
+        model.addAttribute("ingredient", ingredientCommand);
+
+        //init uom
+        ingredientCommand.setUom(new UnitOfMeasureCommand());
+
+        model.addAttribute("uomList",  unitOfMeasureService.listAllUoms());
+
+        return "recipe/ingredient/ingredientform";
     }
 
     @GetMapping
