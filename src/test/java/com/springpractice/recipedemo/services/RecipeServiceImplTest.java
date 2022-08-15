8719ede68bcc64d27459341c428803bf161877c4
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-15 22:31:45 +0530

    test NotFound exception in case not Found id

--- src/test/java/com/springpractice/recipedemo/services/RecipeServiceImplTest.java
+++ src/test/java/com/springpractice/recipedemo/services/RecipeServiceImplTest.java
@@ -1,8 +1,10 @@
 package com.springpractice.recipedemo.services;
 
+import com.springpractice.recipedemo.commands.RecipeCommand;
 import com.springpractice.recipedemo.converters.RecipeCommandToRecipe;
 import com.springpractice.recipedemo.converters.RecipeToRecipeCommand;
 import com.springpractice.recipedemo.domain.Recipe;
+import com.springpractice.recipedemo.exceptions.NotFoundException;
 import com.springpractice.recipedemo.repositories.RecipeRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,10 +23,8 @@ class RecipeServiceImplTest {
     RecipeServiceImpl recipeService;
     @Mock
     RecipeRepository recipeRepository;
-
     @Mock
     RecipeToRecipeCommand recipeToRecipeCommand;
-
     @Mock
     RecipeCommandToRecipe recipeCommandToRecipe;
 
@@ -50,6 +50,39 @@ class RecipeServiceImplTest {
     }
 
     @Test
+    public void getRecipeByIdTestNotFound() throws Exception {
+
+        Optional<Recipe> recipeOptional = Optional.empty();
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+//        Recipe recipeReturned = recipeService.findById(1L);
+
+        Exception exception = assertThrows(NotFoundException.class, () -> { recipeService.findById(1L); });
+        assertTrue(exception.getMessage().contains("Recipe not found"));
+    }
+
+    @Test
+    public void getRecipeCommandByIdTest() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        RecipeCommand recipeCommand = new RecipeCommand();
+        recipeCommand.setId(1L);
+
+        when(recipeToRecipeCommand.convert(any())).thenReturn(recipeCommand);
+
+        RecipeCommand commandById = recipeService.findCommandById(1L);
+
+        assertNotNull(commandById, "Null recipe returned");
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
+    }
+
+    @Test
     void getRecipes() {
         Recipe recipe = new Recipe();
         HashSet recipesData = new HashSet();
@@ -60,6 +93,7 @@ class RecipeServiceImplTest {
         Set<Recipe> recipes = recipeService.getRecipes();
         assertEquals(recipes.size(), 1);
         verify(recipeRepository, times(1)).findAll();
+        verify(recipeRepository, never()).findById(anyLong());
     }
 
     @Test
