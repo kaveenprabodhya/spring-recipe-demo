2e5cee0a0e180abd17e28c47b7beb8b5aa69903d
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-15 23:25:41 +0530

    showById string to long parse correct

--- src/main/java/com/springpractice/recipedemo/controllers/RecipeController.java
+++ src/main/java/com/springpractice/recipedemo/controllers/RecipeController.java
@@ -21,7 +21,7 @@ public class RecipeController {
 
     @GetMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model){
-        model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
+        model.addAttribute("recipe", recipeService.findById(Long.parseLong(id)));
         return "recipe/show";
     }
 
