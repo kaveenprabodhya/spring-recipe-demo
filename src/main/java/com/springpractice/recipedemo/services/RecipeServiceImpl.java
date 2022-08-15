a5eaeafc6f71b0882233f0fc4ea5cd91d6015432
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-15 23:47:31 +0530

    add id to exception in findById

--- src/main/java/com/springpractice/recipedemo/services/RecipeServiceImpl.java
+++ src/main/java/com/springpractice/recipedemo/services/RecipeServiceImpl.java
@@ -40,7 +40,7 @@ public class RecipeServiceImpl implements RecipeService {
         Optional<Recipe> recipeOptional = recipeRepository.findById(l);
         if(!recipeOptional.isPresent()){
 //            throw new RuntimeException("Recipe not found.");
-            throw new NotFoundException("Recipe not found.");
+            throw new NotFoundException("Recipe not found. For id: " + l);
         }
         return recipeOptional.get();
     }
